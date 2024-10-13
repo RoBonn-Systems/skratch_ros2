79ef80a1c4505074f03335303284a340d3d42bb2
harleylara <harley.lara@outlook.com>
2024-10-13 20:20:50 +0200

    add imu support for gazebo fortress

--- skratch_bringup/launch/robot.launch.py
+++ skratch_bringup/launch/robot.launch.py
@@ -144,7 +144,11 @@ def launch_setup(context) -> list[LaunchDescriptionEntity]:
                 executable="spawner",
                 arguments=[
                     "-p", controllers_path_performed,
-                    "joint_state_broadcaster",  # ???? check if we need this
+                    "joint_state_broadcaster",
+                    "front_left_wheel_imu",
+                    "front_right_wheel_imu",
+                    "rear_left_wheel_imu",
+                    "rear_right_wheel_imu"
                 ],
             )
         ],
